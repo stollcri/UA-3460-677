d7ecdd1209ab96aca06955a5495c63816fdca59d
Christopher Stoll <stollcri@gmail.com>
2014-04-16 22:55:43 -0400

    Additional startup code

---
+++ src/loadDocument.c
@@ -0,0 +1,16 @@
+/**
+ * 3460:677 Final Project: Paralelizing OCR using PCA
+ * Christopher Stoll, 2014
+ */
+
+#ifndef LOADDOCUMENT_C
+#define LOADDOCUMENT_C
+
+#include "libpngHelper.c"
+
+void loadDocument(char *fileName)
+{
+	//
+}
+
+#endif
