546d7b62b3c02c2dfa6cb9ad8d10675b99b4120e
Christopher Stoll <stollcri@gmail.com>
2014-04-21 17:22:14 -0400

    Add code to do basic image resizing
    From: http://www.compuphase.com/graphic/scale.htm

---
+++ src/resizeImage.c
@@ -0,0 +1,46 @@
+/**
+ * http://www.compuphase.com/graphic/scale.htm
+ */
+
+void ScaleLine(int *Target, int *Source, int SrcWidth, int TgtWidth)
+{
+  int NumPixels = TgtWidth;
+  int IntPart = SrcWidth / TgtWidth;
+  int FractPart = SrcWidth % TgtWidth;
+  int E = 0;
+
+  while (NumPixels-- > 0) {
+    *Target++ = *Source;
+    Source += IntPart;
+    E += FractPart;
+    if (E >= TgtWidth) {
+      E -= TgtWidth;
+      Source++;
+    } /* if */
+  } /* while */
+}
+
+void resizeImage(int *Source, int *Target, int SrcWidth, int SrcHeight, int TgtWidth, int TgtHeight)
+{
+  int NumPixels = TgtHeight;
+  int IntPart = (SrcHeight / TgtHeight) * SrcWidth;
+  int FractPart = SrcHeight % TgtHeight;
+  int E = 0;
+  int *PrevSource = NULL;
+
+  while (NumPixels-- > 0) {
+    if (Source == PrevSource) {
+      memcpy(Target, Target-TgtWidth, TgtWidth*sizeof(*Target));
+    } else {
+      ScaleLine(Target, Source, SrcWidth, TgtWidth);
+      PrevSource = Source;
+    } /* if */
+    Target += TgtWidth;
+    Source += IntPart;
+    E += FractPart;
+    if (E >= TgtHeight) {
+      E -= TgtHeight;
+      Source += SrcWidth;
+    } /* if */
+  } /* while */
+}
